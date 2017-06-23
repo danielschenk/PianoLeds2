301b12fd1560639b39dbd0782574a148d5da41c2
Daniël Schenk <danielschenk@users.noreply.github.com>
2017-06-24 01:39:53 +0200

    Add license

--- Source/Processing/Interfaces/IRgbFunctionFactory.h
+++ Source/Processing/Interfaces/IRgbFunctionFactory.h
@@ -1,8 +1,20 @@
 /**
  * @file
- * @copyright (c) Daniel Schenk, 2017
+ * @copyright (c) 2017 Daniel Schenk <danielschenk@users.noreply.github.com>
  * This file is part of MLC2.
  * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * @brief Interface for RGB function factories.
  */
 
