4d224d7b023ffc4b7ae0cce26b0b93d08eb6fcff
Daniël Schenk <danielschenk@users.noreply.github.com>
2018-02-17 00:26:15 +0100

    Tryout PlatformIO for nodemcuv2

---
+++ Source/NodeMcuHelloWorld/Main.cpp
@@ -0,0 +1,79 @@
+/**
+ * @file
+ * @copyright (c) Daniel Schenk, 2017
+ * This file is part of MLC2.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ #include "esp_common.h"
+ 
+ /******************************************************************************
+  * FunctionName : user_rf_cal_sector_set
+  * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+  *                We add this function to force users to set rf cal sector, since
+  *                we don't know which sector is free in user's application.
+  *                sector map for last several sectors : ABCCC
+  *                A : rf cal
+  *                B : rf init data
+  *                C : sdk parameters
+  * Parameters   : none
+  * Returns      : rf cal sector
+ *******************************************************************************/
+ extern "C" uint32 user_rf_cal_sector_set(void)
+ {
+     flash_size_map size_map = system_get_flash_size_map();
+     uint32 rf_cal_sec = 0;
+ 
+     switch (size_map) {
+         case FLASH_SIZE_4M_MAP_256_256:
+             rf_cal_sec = 128 - 5;
+             break;
+ 
+         case FLASH_SIZE_8M_MAP_512_512:
+             rf_cal_sec = 256 - 5;
+             break;
+ 
+         case FLASH_SIZE_16M_MAP_512_512:
+         case FLASH_SIZE_16M_MAP_1024_1024:
+             rf_cal_sec = 512 - 5;
+             break;
+ 
+         case FLASH_SIZE_32M_MAP_512_512:
+         case FLASH_SIZE_32M_MAP_1024_1024:
+             rf_cal_sec = 1024 - 5;
+             break;
+         case FLASH_SIZE_64M_MAP_1024_1024:
+             rf_cal_sec = 2048 - 5;
+             break;
+         case FLASH_SIZE_128M_MAP_1024_1024:
+             rf_cal_sec = 4096 - 5;
+             break;
+         default:
+             rf_cal_sec = 0;
+             break;
+     }
+ 
+     return rf_cal_sec;
+ }
+ /******************************************************************************
+  * FunctionName : user_init
+  * Description  : entry of user application, init user function here
+  * Parameters   : none
+  * Returns      : none
+ *******************************************************************************/
+ extern "C" void user_init(void)
+ {
+     printf("SDK version:%s\n", system_get_sdk_version());
+ }
+ 
