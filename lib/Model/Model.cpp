da1bf230f416fca4cf3ce9ba947b3abcbfd5b08a
Daniël Schenk <danielschenk@users.noreply.github.com>
2019-09-03 01:52:34 +0200

    Fix mismatching types

--- lib/Model/Model.cpp
+++ lib/Model/Model.cpp
@@ -28,14 +28,14 @@
 
 #include <utility>
 
-unsigned int Model::subscribe(Model::TUpdateCallback callback)
+Model::UpdateObserverList::TSubscriptionToken Model::subscribe(Model::TUpdateCallback callback)
 {
     std::lock_guard<std::mutex> lock(m_observersMutex);
 
     return m_observers.subscribe(std::move(callback));
 }
 
-void Model::unsubscribe(unsigned int token)
+void Model::unsubscribe(UpdateObserverList::TSubscriptionToken token)
 {
     std::lock_guard<std::mutex> lock(m_observersMutex);
 
